e1e45c2e1b28396f6888bfeb61470c8e85715432
JosTheDude <jos@joscodes.xyz>
2026-03-08 12:40:43 -0400

    feat: copyright to 7587

--- src/main/java/frc/robot/util/AllianceFlipUtil.java
+++ src/main/java/frc/robot/util/AllianceFlipUtil.java
@@ -1,5 +1,5 @@
-// Copyright (c) 2025 FRC 6328
-// http://github.com/Mechanical-Advantage
+// Copyright (c) 2025 Team 7587 Metuchen Momentum
+// https://github.com/frc-team7587
 //
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file at
